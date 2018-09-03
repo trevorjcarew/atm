1b3249a0ab6dd3811de6978c76116166e7fb37cf
trevorjcarew <trevorjcarew@gmail.com>
2018-09-04 00:00:33 +0100

    adding todo to add unit test for 0 quantity notes being returned.

--- src/test/java/com/tc/banking/atm/service/AccountServiceTest.java
+++ src/test/java/com/tc/banking/atm/service/AccountServiceTest.java
@@ -22,92 +22,95 @@ import com.tc.banking.atm.response.BankNoteResponse;
 import com.tc.banking.atm.response.WithdrawCashResponse;
 
 public class AccountServiceTest {
-	
+
 	private static final String NOTE_NAME = "AnyNote";
 	private static final int NUMBER_NOTES = 5;
 	private static final int PIN = 1234;
 	private static final int ACCOUNT = 123456789;
 	private static final double BALANCE = 100.0;
 	private static final double OVERDRAFT = 50.0;
-	
+
 	@Mock
 	private AccountRepository accountRepository;
-	
+
 	@Mock
 	private BankNoteService bankNoteService;
-	
+
 	@InjectMocks
 	private AccountService accountService;
-	
+
 	@Before
-    public void setup() {
-    	MockitoAnnotations.initMocks(this);    	
-    }
-	
+	public void setup() {
+		MockitoAnnotations.initMocks(this);
+	}
+
 	@Test
 	public void testRetrieveFundsIsSuccessful() {
-		when(accountRepository.findByAccountNumber(anyInt())).thenReturn(getAccountEntity());		
+		when(accountRepository.findByAccountNumber(anyInt())).thenReturn(getAccountEntity());
 		AccountCheckResponse response = accountService.retrieveFunds(PIN, ACCOUNT);
 		assertEquals(response.getAvailableFunds(), 150.00 + "");
 		assertEquals(response.getBalance(), 100.00 + "");
 	}
-	
-	@Test (expected = AtmException.class)
+
+	@Test(expected = AtmException.class)
 	public void testAccountNotFoundThrowsAtmException() {
-		when(accountRepository.findByAccountNumber(anyInt())).thenReturn(null);		
+		when(accountRepository.findByAccountNumber(anyInt())).thenReturn(null);
 		accountService.retrieveFunds(PIN, ACCOUNT);
 	}
-	
-	@Test (expected = AtmException.class)
+
+	@Test(expected = AtmException.class)
 	public void testIncorrectPinThrowsAtmException() {
-		when(accountRepository.findByAccountNumber(anyInt())).thenReturn(getAccountEntity());		
-		accountService.retrieveFunds(0000, ACCOUNT);		
+		when(accountRepository.findByAccountNumber(anyInt())).thenReturn(getAccountEntity());
+		accountService.retrieveFunds(0000, ACCOUNT);
 	}
-	
+
 	@Test
 	public void testWithdrawFundsIsSuccessful() {
 		when(accountRepository.findByAccountNumber(anyInt())).thenReturn(getAccountEntity());
 		when(bankNoteService.retrieveRequestedAmount(anyDouble())).thenReturn(getBankNotesList());
 		when(bankNoteService.checkAtmBalance()).thenReturn(200.0);
-		
+
 		WithdrawCashResponse response = accountService.withdrawFunds(PIN, ACCOUNT, 10.0);
-		assertEquals( 90.0, response.getNewBalance(), 10.0);
+		assertEquals(90.0, response.getNewBalance(), 10.0);
 		assertEquals(1, response.getNotes().size());
 		assertEquals(NOTE_NAME, response.getNotes().get(0).getNote());
 		assertEquals(NUMBER_NOTES, response.getNotes().get(0).getNumberOfNotes());
-		
+
 	}
-	
-	@Test (expected = AtmException.class)
+
+	@Test(expected = AtmException.class)
 	public void testUserBalanceInsufficient() {
 		when(accountRepository.findByAccountNumber(anyInt())).thenReturn(getAccountEntity());
 		when(bankNoteService.retrieveRequestedAmount(anyDouble())).thenReturn(getBankNotesList());
 		when(bankNoteService.checkAtmBalance()).thenReturn(200.0);
-		
-		accountService.withdrawFunds(PIN, ACCOUNT, 160.0);		
+
+		accountService.withdrawFunds(PIN, ACCOUNT, 160.0);
 	}
-	
-	@Test (expected = AtmException.class)
+
+	@Test(expected = AtmException.class)
 	public void testAtmBalanceInsufficient() {
 		when(accountRepository.findByAccountNumber(anyInt())).thenReturn(getAccountEntity());
 		when(bankNoteService.retrieveRequestedAmount(anyDouble())).thenReturn(getBankNotesList());
 		when(bankNoteService.checkAtmBalance()).thenReturn(50.0);
-		
-		accountService.withdrawFunds(PIN, ACCOUNT, 80.0);		
+
+		accountService.withdrawFunds(PIN, ACCOUNT, 80.0);
 	}
-	
+
 	@Test
 	public void testUserCanUseOverdraft() {
 		when(accountRepository.findByAccountNumber(anyInt())).thenReturn(getAccountEntity());
 		when(bankNoteService.retrieveRequestedAmount(anyDouble())).thenReturn(getBankNotesList());
 		when(bankNoteService.checkAtmBalance()).thenReturn(200.0);
-		
+
 		WithdrawCashResponse response = accountService.withdrawFunds(PIN, ACCOUNT, 120.0);
 		assertEquals(0, response.getNewBalance(), 100.0);
 		assertEquals(1, response.getNotes().size());
 		assertEquals(NOTE_NAME, response.getNotes().get(0).getNote());
-		assertEquals(NUMBER_NOTES, response.getNotes().get(0).getNumberOfNotes());		
-	}	
+		assertEquals(NUMBER_NOTES, response.getNotes().get(0).getNumberOfNotes());
+	}
+
+	// TODO Add test to check that notes with a quantity of 0 are not returned in
+	// response
 
 	private AccountEntity getAccountEntity() {
 		AccountEntity entity = new AccountEntity();
@@ -117,10 +120,10 @@ public class AccountServiceTest {
 		entity.setPin(PIN);
 		return entity;
 	}
-	
-	private List<BankNoteResponse> getBankNotesList(){
+
+	private List<BankNoteResponse> getBankNotesList() {
 		List<BankNoteResponse> bankNoteList = new ArrayList<>();
-		
+
 		BankNoteResponse bankNote = new BankNoteResponse();
 		bankNote.setNote(NOTE_NAME);
 		bankNote.setNumberOfNotes(NUMBER_NOTES);
