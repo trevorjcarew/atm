d662cd8ec512f75d139eaa958e2c0ee68899773b
trevorjcarew <trevorjcarew@gmail.com>
2018-09-03 23:47:12 +0100

    fix for bug where notes with a 0 quantity are being returned.

--- src/main/java/com/tc/banking/atm/service/AccountService.java
+++ src/main/java/com/tc/banking/atm/service/AccountService.java
@@ -1,5 +1,6 @@
 package com.tc.banking.atm.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,9 +21,9 @@ public class AccountService {
 
 	@Autowired
 	private AccountRepository accountRepository;
-	
+
 	@Autowired
-	private BankNoteService bankNoteService;	
+	private BankNoteService bankNoteService;
 
 	public AccountCheckResponse retrieveFunds(int pin, int accountNumber) {
 		AccountCheckResponse response = new AccountCheckResponse();
@@ -33,12 +34,12 @@ public class AccountService {
 		return response;
 	}
 
-	public WithdrawCashResponse withdrawFunds(int pin, int accountNumber, double amount) {		
-		AccountEntity accountEntity = retrieveAccount(accountNumber);		
-		validatePin(pin, accountEntity.getPin());		
-		verifyUserBalance(amount, accountEntity.getBalance() + accountEntity.getOverdraft());		
-		verifyAtmBalance(amount);		
-		List<BankNoteResponse> retrievedNotes = bankNoteService.retrieveRequestedAmount(amount);		
+	public WithdrawCashResponse withdrawFunds(int pin, int accountNumber, double amount) {
+		AccountEntity accountEntity = retrieveAccount(accountNumber);
+		validatePin(pin, accountEntity.getPin());
+		verifyUserBalance(amount, accountEntity.getBalance() + accountEntity.getOverdraft());
+		verifyAtmBalance(amount);
+		List<BankNoteResponse> retrievedNotes = bankNoteService.retrieveRequestedAmount(amount);
 		double newBalance = updateAccountAndGetBalance(accountEntity, amount);
 		return formWithdrawelResponse(retrievedNotes, newBalance);
 	}
@@ -47,19 +48,19 @@ public class AccountService {
 		double balance = accountEntity.getBalance();
 		double overdraft = accountEntity.getOverdraft();
 
-		//when overdrawn set balance to 0 and update overdraft amount
+		// when overdrawn set balance to 0 and update overdraft amount
 		if (amount > balance) {
 			double overdraftReduction = amount - balance;
 			accountEntity.setOverdraft(overdraft - overdraftReduction);
 			accountEntity.setBalance(0.0);
 		} else {
 			accountEntity.setBalance(balance - amount);
-		}		
+		}
 		accountRepository.save(accountEntity);
-		
+
 		return accountEntity.getBalance();
 	}
-	
+
 	private AccountEntity retrieveAccount(int accountNumber) {
 		AccountEntity entity = accountRepository.findByAccountNumber(accountNumber);
 		if (entity == null) {
@@ -69,33 +70,36 @@ public class AccountService {
 		return entity;
 	}
 
+	// TODO temporary bugFix: this could be better implemented by not including the
+	// non zero notes in the first place
 	private WithdrawCashResponse formWithdrawelResponse(List<BankNoteResponse> retrievedNotes, double newBalance) {
-		WithdrawCashResponse response  = new WithdrawCashResponse();
-		
+		WithdrawCashResponse response = new WithdrawCashResponse();
+
 		response.setNewBalance(newBalance);
-		
-		//remove notes with a quantity of 0 from response
+		List<BankNoteResponse> nonZeroBankNotes = new ArrayList<>();
+
+		// remove notes with a quantity of 0 from response
 		for (int i = 0; i < retrievedNotes.size(); i++) {
-			if(retrievedNotes.get(i).getNumberOfNotes() == 0) {
-				retrievedNotes.remove(i);
+			if (retrievedNotes.get(i).getNumberOfNotes() != 0) {
+				nonZeroBankNotes.add(retrievedNotes.get(i));
 			}
 		}
-		response.setNotes(retrievedNotes);
-		
+		response.setNotes(nonZeroBankNotes);
+
 		return response;
 	}
-	
+
 	private void verifyAtmBalance(double requestedAmount) {
-		//get all notes from the db and sum up their total
+		// get all notes from the db and sum up their total
 		Double atmBalance = bankNoteService.checkAtmBalance();
 		if (requestedAmount > atmBalance) {
-			log.error("requestedAmout - "  + requestedAmount + ", atmBalanace - " + atmBalance);
+			log.error("requestedAmout - " + requestedAmount + ", atmBalanace - " + atmBalance);
 			throw new AtmException("Unable to dispense this amount - Choose a lower amount");
 		}
 	}
 
 	private void validatePin(int pin, int entityPin) {
-		//check if pin provided matches pin in db
+		// check if pin provided matches pin in db
 		if (pin != entityPin) {
 			log.error("pin number entered doesn't match account pin");
 			throw new AtmException("Invalid pin number entered");
@@ -103,7 +107,7 @@ public class AccountService {
 	}
 
 	private void verifyUserBalance(double amount, double availableFunds) {
-		//check that the have enough funds in their account
+		// check that the have enough funds in their account
 		if (amount > availableFunds) {
 			log.error("requestedAmount - " + amount + ", availableFunds - " + availableFunds);
 			throw new AtmException("Insufficient funds");
